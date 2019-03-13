80d9cdeb0a1a489991254e71dcacd6d0eba83300
Peter Murasky <pmurasky@ford.com>
2019-03-13 09:49:48 -0400

    added exercise component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ExerciseComponent} from './components/exercise/exercise.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'exercise', component: ExerciseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExerciseComponent } from './components/exercise/exercise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExerciseComponent
   ],
   imports: [
     BrowserModule,
